2e8489bbff2b53cea94e78cd8c0d8df77565785d
Tony <applepies16m@gmail.com>
2026-07-23 12:16:22 +1000

    Update index.js
    partner changes

--- Task 1/index.js
+++ Task 1/index.js
@@ -1,5 +1,5 @@
 // Use the command prompt rather than powershell by right clicking on the "+" icon.
-console.log("Hello World!");
+console.log("I caused a conflict !");
 // note that to run this inside the Week 2 folder, change directory to Task 1.
                                 // cd "Task 1"
    // change back to the parent directory (Week 2) when needed with "cd .."
